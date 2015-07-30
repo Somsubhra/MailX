8e0a5a1753ce7b609d90d0054e30260fa67b55d2
Somsubhra Bairi <somsubhra.b@endurance.com>
2015-07-30 11:58:59 +0530

    Provision to link www root

--- client/setup.sh
+++ client/setup.sh
@@ -5,3 +5,8 @@ apt-get install -y php5 libapache2-mod-php5
 
 /etc/init.d/apache2 restart
 php -r 'echo "\n\nYour PHP installation is working fine.\n\n\n";'
+
+if ! [ -L /var/www ]; then
+  rm -rf /var/www
+  ln -fs /vagrant/client/www /var/www
+fi
