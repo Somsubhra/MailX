b5757eb6ea893b5987368072145a0f90f3784957
Somsubhra Bairi <somsubhra.b@endurance.com>
2015-07-30 14:52:52 +0530

    Fetch and insert email addresses and corresponding namespace ids into database

--- client/bin/update_db.php
+++ client/bin/update_db.php
@@ -3,7 +3,7 @@
 include "../etc/config.php";
 error_reporting(E_ERROR | E_PARSE);
 
-echo "Pulling in new email accounts...\n";
+echo "Pulling in new email accounts\n";
 
 $json_file_content = file_get_contents(API_ROOT . "n");
 $json_result = json_decode($json_file_content);
@@ -14,10 +14,61 @@ if($db->connect_errno > 0) {
 	die("Unable to connect to MailX database: " . $db->connect_error);
 }
 
-foreach($json_result as $json_record) {
-	var_dump($json_record);
+if(!($count_statement = $db->prepare("SELECT COUNT(*) FROM email_namespace_mapping WHERE email_address = ? AND namespace_id = ?"))) {
+	die("Count statement preparation failed: " . $count_statement->error . "\n");
 }
 
+$new_email_namespace_mappings = array();
+
+foreach($json_result as $json_record) {
+
+	$email_address = $json_record->email_address;
+	$namespace_id = $json_record->namespace_id;
+
+	if(!$count_statement->bind_param("ss", $email_address, $namespace_id)) {
+		die("Parameters binding on count statement failed: " . $count_statement->error . "\n");
+	}
+
+	if(!$count_statement->execute()) {
+		die("Count statement execution failed: " . $count_statement->error . "\n");	
+	}
+
+	$mapping_count = 0;
+
+	$out_mapping_count = NULL;
+
+	if(!$count_statement->bind_result($out_mapping_count)) {
+		die("Output binding on count statement failed: " . $count_statement->error . "\n");
+	}
+
+	while($count_statement->fetch()) {
+		$mapping_count = $out_mapping_count;
+	}
+
+	if($mapping_count == 0) {
+		$new_email_namespace_mappings[$email_address] = $namespace_id;
+	}
+}
+
+$count_statement->close();
+
+if(!($insert_statement = $db->prepare("INSERT INTO email_namespace_mapping (email_address, namespace_id) VALUES(?, ?)"))) {
+	die("Insert statement preparation failed: " . $insert_statement->error . "\n");
+}
+
+foreach ($new_email_namespace_mappings as $email_address => $namespace_id) {
+	if(!$insert_statement->bind_param("ss", $email_address, $namespace_id)) {
+		die("Parameters binding on insert statement failed: " . $insert_statement->error . "\n");
+	}
+
+	if(!$insert_statement->execute()) {
+		die("Insert statement execution failed: " . $insert_statement->error . "\n");
+	}
+
+	echo "Added " . $email_address . "\n";
+}
+
+$insert_statement->close();
 $db->close();
 
-echo "Exiting...\n";
+echo "Exiting\n";
