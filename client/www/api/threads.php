8ed43a871b88c92bc2ee7550a400db921202e8e6
Somsubhra Bairi <somsubhra.b@endurance.com>
2015-07-31 12:35:13 +0530

    Added barebones to the threads API

--- client/www/api/threads.php
+++ client/www/api/threads.php
@@ -3,3 +3,24 @@ header("Content-Type: application/json");
 
 include "../../etc/config.php";
 include "libs/error.php";
+
+if(!isset($_GET["api_key"])) {
+    show_invalid_key_error();
+}
+
+$api_key = $_GET["api_key"];
+
+$db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+if($db->connect_errno > 0) {
+    show_db_error();
+}
+
+echo json_encode(array(
+    "success" => true,
+    "body" => array(
+        "threads" => array()
+    )
+));
+
+$db->close();
