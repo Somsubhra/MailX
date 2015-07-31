c9639f1ac9ad7bc3e28fa0462200fc15d710b1c9
Somsubhra Bairi <somsubhra.b@endurance.com>
2015-07-31 12:42:17 +0530

    Fetch namespace ids for the threads API

--- client/www/api/threads.php
+++ client/www/api/threads.php
@@ -2,6 +2,7 @@
 header("Content-Type: application/json");
 
 include "../../etc/config.php";
+include "libs/auth.php";
 include "libs/error.php";
 
 if(!isset($_GET["api_key"])) {
@@ -16,9 +17,35 @@ if($db->connect_errno > 0) {
     show_db_error();
 }
 
+$account_id = get_account_id_from_api_key($api_key, $db);
+
+if(!($select_statement = $db->prepare("SELECT namespace_id FROM email_namespace_mapping WHERE mailx_account_id = ?"))) {
+    show_db_error();
+}
+
+if(!$select_statement->bind_param("s", $account_id)) {
+    show_db_error();
+}
+
+if(!$select_statement->execute()) {
+    show_db_error();
+}
+
+$namespace_ids = array();
+$out_namespace_id = NULL;
+
+if(!$select_statement->bind_result($out_namespace_id)) {
+    show_db_error();
+}
+
+while($select_statement->fetch()) {
+    array_push($namespace_ids, $out_namespace_id);
+}
+
 echo json_encode(array(
     "success" => true,
     "body" => array(
+        "namespace_ids" => $namespace_ids,
         "threads" => array()
     )
 ));
