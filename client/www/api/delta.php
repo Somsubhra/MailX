3872d516919273accbcd1c47216159fcf5319823
Somsubhra Bairi <somsubhra.b@endurance.com>
2015-08-01 05:17:39 +0530

    Get the Delta API working.

--- client/www/api/delta.php
+++ client/www/api/delta.php
@@ -30,4 +30,14 @@ if($db->connect_errno > 0) {
 $account = get_account_from_api_key($api_key, $db);
 $namespace_id = $account["namespace_id"];
 
+$deltas_json_content = file_get_contents(API_ROOT . "n/$namespace_id/delta?cursor=$cursor");
+$deltas_json = json_decode($deltas_json_content);
+
+echo json_encode(array(
+    "success" => true,
+    "body" => array(
+        "deltas" => $deltas_json
+    )
+));
+
 $db->close();
