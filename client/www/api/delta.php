ef6a35e51dc216a6843ba546af4f97c2814af3d8
Somsubhra Bairi <somsubhra.b@endurance.com>
2015-08-01 05:12:13 +0530

    Added barebones of the delta API

---
+++ client/www/api/delta.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * /api/delta.php?api_key=<api_key>&cursor=<cursor>
+ */
+
+header("Content-Type: application/json");
+
+include "../../etc/config.php";
+include "libs/auth.php";
+include "libs/error.php";
+
+if(!isset($_GET["api_key"])) {
+    show_invalid_key_error();
+}
+
+$api_key = $_GET["api_key"];
+
+if(!isset($_GET["cursor"])) {
+    show_invalid_params_error();
+}
+
+$cursor = $_GET["cursor"];
+
+$db = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+if($db->connect_errno > 0) {
+    show_db_error();
+}
+
+$account = get_account_from_api_key($api_key, $db);
+$namespace_id = $account["namespace_id"];
+
+$db->close();
