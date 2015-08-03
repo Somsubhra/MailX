928bb791d2cb4797bd1d687bbb85d841909ac9cb
Somsubhra Bairi <somsubhra.b@endurance.com>
2015-08-03 16:28:18 +0530

    Fetch contacts

--- client/www/static/js/mailx.js
+++ client/www/static/js/mailx.js
@@ -5,8 +5,26 @@ function set_api_key(param_api_key, callback) {
     callback();
 }
 
+function get_contact_display_name(contact) {
+    if(contact.name == "") {
+        return contact.email;
+    }
+    return contact.name;
+}
+
 function load_contacts() {
     console.log("Contacts API key: " + api_key);
+    $.get("api/contacts.php",
+        {
+            api_key: api_key
+        }, function(data) {
+            var contacts = data.body.contacts;
+            var num_contacts = contacts.length;
+
+            for(var i = 0; i < num_contacts; i++) {
+                console.log(get_contact_display_name(contacts[i]));
+            }
+        }, "json");
 }
 
 function load_preview() {
