6f8050fdde6ad1563f43dd47a2b73cf37f5c6d0c
Somsubhra Bairi <somsubhra.b@endurance.com>
2015-08-03 11:42:47 +0530

    Provision for php curl API

--- client/setup.sh
+++ client/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 apt-get install -y apache2
-apt-get install -y php5 libapache2-mod-php5 php5-mysql
+apt-get install -y php5 libapache2-mod-php5 php5-mysql php5-curl
 
 /etc/init.d/apache2 restart
 php -r 'echo "\n\nYour PHP installation is working fine.\n\n\n";'
