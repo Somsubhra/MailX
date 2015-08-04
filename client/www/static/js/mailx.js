186b1c45d22c51761df792858d8e8cd769137e1b
Somsubhra Bairi <somsubhra.b@endurance.com>
2015-08-04 13:06:05 +0530

    Convert timestamp to local time function

--- client/www/static/js/mailx.js
+++ client/www/static/js/mailx.js
@@ -15,6 +15,26 @@ function shorten_string(string, length) {
     return string;
 }
 
+function timestamp_to_localtime(timestamp) {
+    var d = new Date(timestamp * 1000),
+        yyyy = d.getFullYear(),
+        mm = ('0' + (d.getMonth() + 1)).slice(-2),
+        dd = ('0' + d.getDate()).slice(-2),
+        hh = d.getHours(),
+        h = hh,
+        min = ('0' + d.getMinutes()).slice(-2),
+        ampm = 'AM',
+        time;
+    if (hh > 12) {
+        h = hh - 12;
+        ampm = 'PM';
+    } else if (hh == 0) {
+        h = 12;
+    }
+    time = yyyy + '-' + mm + '-' + dd + ', ' + h + ':' + min + ' ' + ampm;
+    return time;
+}
+
 function get_contact_display_name(contact) {
     if(contact.name == "") {
         return contact.email;
